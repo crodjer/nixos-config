6c46db3aa11503a0046a9c576ae31870b77548f7
Rohan Jain <crodjer@pm.me>
2023-06-29 17:04:10 +0530

    Update Vim / Tmux configs to match crodjer/configs

--- tmux.nix
+++ tmux.nix
@@ -18,8 +18,7 @@
       set-option -g set-titles on
       set-option -g mouse on
 
-      # I use alacritty everywhere
-      set -g default-terminal "alacritty"
+      set -g default-terminal "$TERM"
 
       ## Copy mode
       # Use vim keybindings in copy mode
@@ -34,6 +33,20 @@
       bind-key -n M-\' split-window -v
       bind-key -n M-\\ split-window -h
       bind-key -n M-F resize-pane -Z
+      bind-key z last-window
+      bind-key -n M-z last-window
+
+      # Select windows by number
+      bind -n M-0 select-window -t 0
+      bind -n M-1 select-window -t 1
+      bind -n M-2 select-window -t 2
+      bind -n M-3 select-window -t 3
+      bind -n M-4 select-window -t 4
+      bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t 9
 
       # Window Navigation
       bind-key -n M-j select-pane -L
@@ -43,16 +56,21 @@
 
       # Theme
       set -g status-fg white
-      set -g status-bg colour8
-      set -g status-left '#[fg=black,bg=color15] #S '
-      set -g window-status-format "#[fg=white] #I #W:#{s|$HOME|~|;s|/.*/|/…/|:pane_current_path} "
-      set -g window-status-current-format "#[fg=black,bg=colour7,bold] #I #W:#{s|$HOME|~|;s|/.*/|/…/|:pane_current_path} "
-      set -g status-right '#[fg=black] #{cpu_temp_bg_color}  #{cpu_temp} #{cpu_bg_color}  #{cpu_percentage} #{ram_bg_color}  #{ram_percentage} #[fg=black,bg=color15]  %H:%M '
+      set -g status-bg black
+
+      set -g window-status-current-style 'fg=black,bg=brightyellow,bold'
+      set -g window-status-bell-style 'fg=black,bg=red,blink'
+      set -g window-status-separator ""
+      # Substitue Home Directory with ~ and any nested paths with …/
+      # Also, substitute zsh and bash shells with $
+      set -g window-status-format " #I #{s|zsh|$|;s|bash|$|:pane_current_command}:#{s|$HOME|~|;s|/.*/|…/|:pane_current_path} "
+      set -Fg window-status-current-format "#{window-status-format}"
+      set -g status-left "#[fg=black,bg=brightyellow] #S "
+      set -g status-right "#[fg=black]#{cpu_bg_color} C:#{cpu_percentage} #{ram_bg_color} M:#{ram_percentage}  #[bg=brightyellow] %H:%e"
 
       run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
     '';
     plugins = with pkgs.tmuxPlugins; [
-      cpu
     ];
   };
 
