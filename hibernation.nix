56fd22561814b96d6f48caf5f86196938e8847b5
Rohan Jain <code@rohanjain.in>
2023-12-31 20:40:46 +0530

    Script to generate hibernation config

--- hibernation.nix
+++
@@ -1,9 +0,0 @@
-{
-  # This is a template to configure hiberation when needed.
-  # Set the size according to the system RAM
-  swapDevices = [ { device = "/var/swapfile"; size = 8192; } ];
-  # Get this using: filefrag -v /var/swapfile
-  boot.kernelParams = ["resume_offset=<offset>"];
-  # The uuid for `/` partition.
-  boot.resumeDevice = "/dev/disk/by-uuid/<uuid-for-root-partition>";
-}
