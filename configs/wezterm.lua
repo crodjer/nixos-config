1c01a1be054ca92cb04ff3b748745c2ed58382b6
Rohan Jain <code@rohanjain.in>
2024-01-13 15:21:01 +0530

    Add Wezterm and Some sway tewaks

---
+++ configs/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font_with_fallback {
+  "Hack Nerd Font"
+}
+config.color_scheme = "Catppuccin Latte"
+
+return config
