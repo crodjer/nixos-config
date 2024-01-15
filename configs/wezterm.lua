0baa75601e71b9c81ccff3a19cdd540519cc8dff
Rohan Jain <code@rohanjain.in>
2024-01-15 22:34:12 +0530

    Don't hide cursor when typing

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm'
-local act = wezterm.action
 
 local config = wezterm.config_builder()
 
 config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
+config.hide_mouse_cursor_when_typing = false
 
 config.font_size = 10
 config.font = wezterm.font_with_fallback {
