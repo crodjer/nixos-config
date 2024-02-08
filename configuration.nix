781217f4b88ad407f2ae7c1b2461e721a9ed2c69
Rohan Jain <code@rohanjain.in>
2024-02-08 22:03:47 +0530

    Add machine.nix for optional machine configuration

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,8 @@
     /etc/nixos/hardware-configuration.nix
     ./nixos-base.nix
     ./home.nix
-  ] ++ lib.optional (builtins.pathExists /etc/nixos/swap.nix) /etc/nixos/swap.nix
-    ++ lib.optional (builtins.pathExists /etc/nixos/local.nix) /etc/nixos/local.nix;
+  ]
+  ++ lib.optional (builtins.pathExists /etc/nixos/machine.nix) /etc/nixos/machine.nix
+  ++ lib.optional (builtins.pathExists /etc/nixos/swap.nix) /etc/nixos/swap.nix
+  ++ lib.optional (builtins.pathExists /etc/nixos/local.nix) /etc/nixos/local.nix;
 }
--- install.nix
+++ install.nix
@@ -5,6 +5,8 @@
     # Include the results of the hardware scan.
     /mnt/etc/nixos/hardware-configuration.nix
     ./nixos-base.nix
-  ] ++ lib.optional (builtins.pathExists /etc/nixos/swap.nix) /etc/nixos/swap.nix
-    ++ lib.optional (builtins.pathExists /etc/nixos/local.nix) /etc/nixos/local.nix;
+  ]
+  ++ lib.optional (builtins.pathExists /etc/nixos/machine.nix) /etc/nixos/machine.nix
+  ++ lib.optional (builtins.pathExists /etc/nixos/swap.nix) /etc/nixos/swap.nix
+  ++ lib.optional (builtins.pathExists /etc/nixos/local.nix) /etc/nixos/local.nix;
 }
