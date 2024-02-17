9f544631ab917353411f36fbad0acf0e55987633
Rohan Jain <code@rohanjain.in>
2024-02-17 12:14:05 +0530

    Remove atuin

--- home.nix
+++ home.nix
@@ -9,7 +9,6 @@ in {
   home-manager.users.${user_name} = {
     home = {
       file = {
-        ".config/atuin/config.toml".source = ./configs/atuin.toml;
         ".config/helix".source = ./configs/helix;
         ".config/mako/config".source = ./configs/mako;
         ".config/wofi".source = ./configs/wofi;
