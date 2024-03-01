d18262b018a3f5ad36ecd84d8813ef6183880666
Rohan Jain <crodjer@proton.me>
2024-03-01 16:46:53 +0530

    Add `hugo` my static site generator

--- nixos-base.nix
+++ nixos-base.nix
@@ -111,7 +111,7 @@ in {
   environment = {
     systemPackages = with pkgs; [
       # Cli utilities
-      bat bc bottom entr eza fd fzf git jq ripgrep unzip xdg-utils
+      bat bc bottom entr eza fd fzf git hugo jq ripgrep unzip xdg-utils
       zsh-completions zoxide
 
       # Applications
