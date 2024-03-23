ad1467216ae9a14ec02ee160b02b1a19ebea889f
Rohan Jain <code@rohanjain.in>
2024-03-23 12:13:14 +0530

    Custom bat style

--- configs/sway/env.sh
+++ configs/sway/env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 export BAT_THEME=GitHub
+export BAT_STYLE=grid,header,header-filesize,header-filename
+
 export GOPATH="$HOME/.local/share/go"
 export PATH="$HOME/.cargo/bin:$PATH"
